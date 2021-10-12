c7d56ba9a12717b073f9e795ce7aa3167cc59c37
AnYelg <73191386+AnYelg@users.noreply.github.com>
2021-10-11 20:12:21 -0500

    Create reto2.cpp

---
+++ reto2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+//Andrea Yela González A01025250
+//Joshua Rúben Amaya Camilo A01025258
+
+class ConexionesComputadora{
+    public:
+        int IP;
+        string Nombre;
+        void ConexionesEntrantes(){
+
+        }
+        void ConexionesSalientes(){
+            
+        }
+};
