2927f86612dbb3910cd7843716ed10cfd11b2a35
MaxJoshua <A01025258@itesm.mx>
2021-11-30 14:04:37 -0600

    Update reto5_1.cpp

--- reto5_1.cpp
+++ reto5_1.cpp
@@ -3,7 +3,7 @@
 #include <stack>
 #include <list>
 #include <unordered_map>
-
+#include <queue>
 using namespace std;
 
 template <class T>
@@ -69,51 +69,51 @@ class Grafo
 		}
 	}
 
-    bool DFS(T nodo_primero, T nodo_segundo){
-        stack<Nodo<T>*> visitIn;
-        visitIn.push(nodos[nodo_primero]);
-        Nodo<T>* temp = visitIn.top();
+    bool DFS(T nodo1, T nodo2){
+        stack<Nodo<T>*> visitar;
+        visitar.push(nodos[nodo1]);
+        Nodo<T>* temp = visitar.top();
 
-        while((!visitIn.empty())){
-            if(visitIn.top()->id == nodo_segundo){
+        while(!visitar.empty()){
+            if(visitar.top()->id == nodo2){
                 return true;
             }
 
-            temp = visitIn.top();
-            visitIn.pop();
+            temp = visitar.top();
+            visitar.pop();
 
-            for(auto it : temp -> siguiente){
-                if(it.first->visitIn == false){
-                    visitIn.push(it.first);
+            for(auto it : temp->siguientes){
+                if(it.first->visitado == false){
+                    visitar.push(it.first);
                 }
             }
 
-            temp->visitIn = true;
+            temp->visitado = true;
         }
 
         return false;
     }
 
     bool BFS(T nodo_primero, T nodo_segundo){
-        stack<Nodo<T>*> visitIn;
-        visitIn.push(nodos[nodo_primero]);
-        Nodo<T>* temp = visitIn.front();
+        queue<Nodo<T>*> visitar;
+        visitar.push(nodos[nodo_primero]);
+        Nodo<T>* temp = visitar.front();
 
-        while((!visitIn.empty())){
-            if(visitIn.front()->id == nodo_segundo){
+        while(!visitar.empty()){
+            if(visitar.front()->id == nodo_segundo){
                 return true;
             }
 
-            temp = visitIn.front();
-            visitIn.pop();
+            temp = visitar.front();
+            visitar.pop();
 
-            for(auto it : temp -> siguiente){
-                if(it.first->visitIn == false){
-                    visitIn.push(it.first);
+            for(auto it : temp -> siguientes){
+                if(it.first->visitado == false){
+                    visitar.push(it.first);
                 }
             }
 
-            temp->visitIn = true;
+            temp->visitado = true;
         }
 
         return false;
@@ -158,9 +158,23 @@ int main(){
     g.agregarArcoDirigidoConPeso("Z", "S", 9);
     g.agregarArcoDirigidoConPeso("S", "N", 2);
 
-    g.DFS("D", "N");
+    bool solucion = g.DFS("A", "J");
+    if(solucion == true){
+        cout << "Existe solución" << endl;
+    }
+    else{
+        cout << "No existe solución" << endl;
+    }
 
-    g.imprimir();
+
+    bool solucion2 = g.BFS("A", "J");
+    if(solucion2 == true){
+        cout << "Existe solución" << endl;
+    }
+
+    else{
+        cout << "No existe solución" << endl;
+    }
 
     return 0;
 }
