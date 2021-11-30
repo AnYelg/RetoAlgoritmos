554e17e9c6191340e31ed3cbcf0051f09baa6fd4
MaxJoshua <A01025258@itesm.mx>
2021-11-30 09:07:03 -0600

    Ya compara el primero

--- reto5.cpp
+++ reto5.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <stack>
+#include <queue>
 #include <list>
 #include <unordered_map>
 
@@ -51,6 +53,31 @@ class Record
 		
 };
 
+vector<Record> data;
+void cargarDatos()
+{
+	ifstream in;
+	in.open("datosEquipo5.csv");
+	string line, parte;
+	vector<string> partes;
+	while(getline(in, line))
+    {
+		if(line.find("\r")!=line.npos)
+        {
+				line=line.substr(0, line.size()-1);
+		}
+		istringstream inString(line);
+		while(getline(inString, parte, ','))
+        {
+			partes.push_back(parte);
+		}
+		Record r(partes[0], partes[1], partes[2], partes[3], partes[4], partes[5], partes[6], partes[7]);
+		data.push_back(r);
+		partes.clear();
+	}
+	in.close();
+}
+
 template <class T>
 class Nodo{
 	public:
@@ -78,12 +105,18 @@ class Nodo{
 	}
 };
 
+
 template <class T>
 class Grafo
 {
 	unordered_map<T,Nodo<T>*> nodos;
 	
 	public:
+
+	//int leerSiguiente(T d){
+
+	//}
+
 	
 	void agregarNodo(T id)
 	{
@@ -112,9 +145,35 @@ class Grafo
 		}
 	}
 	
+	
 };
 
 int main()
 {
+	cargarDatos();
+
+	Grafo<string> ConexionesUno;
+	int peso=0;
+	
+	
+	
+	string dia= "10-8-2020";
+
+
+	for(auto it : data){
+		if(it.fecha == dia){
+			if(it.ipDestino.find("172.21.65.") != string::npos){
+				ConexionesUno.agregarNodo(it.ipDestino);
+			}
+
+			if(it.ipFuente.find("172.21.65.") != string::npos){
+				ConexionesUno.agregarNodo(it.ipFuente);
+			}
+
+		ConexionesUno.agregarArcoNoDirigidoConPeso(it.ipFuente, it.ipDestino, peso);
+		}
+	}
+
+	ConexionesUno.imprimir();
 
 }
